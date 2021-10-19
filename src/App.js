124d0a9e8acea9e6148e5d3368b28a357669f06a
CodeCostylProduction <starostin039@gmail.com>
2021-10-19 21:46:21 +0300

    up to 69 exercise

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import {Route,NavLink, Switch, Redirect} from "react-router-dom";
 import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
+
+    state = {
+        isLoggedIn: false,
+    }
+
   render() {
 
     return (
@@ -37,13 +42,20 @@ class App extends Component {
 
           </ul>
         </nav>
-
+        <hr/>
+        <div style={{textAlign: 'center'}}>
+            <h3>Is logged in {this.state.isLoggedIn ? 'True' : 'False'}</h3>
+              <button onClick={() => this.setState({isLoggedIn: !this.state.isLoggedIn})}>Sign In</button>
+        </div>
         <hr/>
         <Switch>
         <Route path="/" exact render={() => <h1>Home page</h1>}/>
         <Route path="/about" exact component={About}/>
-        <Route path="/Cars/:name" component={CarDetail} />
-        <Route path="/Cars" component={Cars}/>
+        <Route path="/Cars/:name" component={CarDetail}/>
+            {this.state.isLoggedIn ?
+                <Route path="/Cars" component={Cars}/>
+                : null
+            }
         <Redirect to={'/'}/>
         {/*<Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>}/>*/}
         </Switch>
